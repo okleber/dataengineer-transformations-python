74fd4289d8261dd88af2da27f92524a3f595fbc6
povoacao <povoacao@localhost>
2021-10-19 22:52:55 +0200

    implementado wordcount

--- data_transformations/wordcount/word_count_transformer.py
+++ data_transformations/wordcount/word_count_transformer.py
@@ -1,12 +1,22 @@
 import logging
+import re
 
 from pyspark.sql import SparkSession
 
 
+def regex_pattern() -> re.Pattern:
+    DELIMITER = re.compile(''',(?=(?:[^"]*"[^"]*")*[^"]*$)''')
+    return DELIMITER
+
+
 def run(spark: SparkSession, input_path: str, output_path: str) -> None:
+    delimiter = regex_pattern()
     logging.info("Reading text file from: %s", input_path)
-    input_df = spark.read.text(input_path)
-
+    words = spark.sparkContext.textFile(input_path)\
+        .flatMap(lambda line: delimiter
+                 .split(line))
+    word_count = words.map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
     logging.info("Writing csv to directory: %s", output_path)
-
-    input_df.coalesce(1).write.csv(output_path, header=True)
+    df = spark.createDataFrame(word_count).toDF("word", "count")
+    df.coalesce(1).write.csv(output_path, header=True)
