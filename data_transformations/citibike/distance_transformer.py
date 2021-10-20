970e7af09c18809342610977c5f12f78c1132479
povoacao <povoacao@localhost>
2021-10-20 20:19:42 +0200

    ainda testando distancia

--- data_transformations/citibike/distance_transformer.py
+++ data_transformations/citibike/distance_transformer.py
@@ -1,20 +1,27 @@
-from pyspark.sql import SparkSession, DataFrame
+import logging
 
-METERS_PER_FOOT = 0.3048
-FEET_PER_MILE = 5280
-EARTH_RADIUS_IN_METERS = 6371e3
-METERS_PER_MILE = METERS_PER_FOOT * FEET_PER_MILE
-
-
-def compute_distance(_spark: SparkSession, dataframe: DataFrame) -> DataFrame:
-    return dataframe
+from pyspark.sql import SparkSession, DataFrame, Column
+from typing import List
+from pyspark.sql.functions import col
+from haversine import haversine, Unit
 
 
-def run(spark: SparkSession, input_dataset_path: str, transformed_dataset_path: str) -> None:
-    input_dataset = spark.read.parquet(input_dataset_path)
-    input_dataset.show()
+def compute_row(row) -> Column:
+    start = (col('start_station_latitude'), col('df.start_station_longitude'))
+    end = (col('end_station_latitude'), col('df.end_station_longitude'))
+    row.distance = col(str(haversine(start, end, unit=Unit.MILES)))
+    return row.distance
 
-    dataset_with_distances = compute_distance(spark, input_dataset)
-    dataset_with_distances.show()
 
-    dataset_with_distances.write.parquet(transformed_dataset_path, mode='append')
+def compute_distance(_spark: SparkSession, df: DataFrame) -> DataFrame:
+    df.withColumn('distance', compute_row(df))
+    return df
+
+
+def run(spark: SparkSession, input_dataset_path: List[str], transformed_dataset_path: List[str]) -> None:
+    for file in input_dataset_path:
+        input_dataset = spark.read.parquet(file)
+        input_dataset.show()
+        dataset_with_distances = compute_distance(spark, input_dataset)
+        dataset_with_distances.show()
+        # dataset_with_distances.write.parquet(transformed_dataset_path, mode='append')
--- executa.sh
+++ executa.sh
@@ -3,8 +3,8 @@ set -x
 >project.log
 poetry build
 #poetry run spark-submit --master local --py-files dist/data_transformations-*.whl jobs/word_count.py resources/word_count/words.txt out/word_count
-poetry run spark-submit --master local --py-files dist/data_transformations-*.whl jobs/citibike_ingest.py resources/citibike/*.csv out/citibike
-#poetry run spark-submit --master local --py-files dist/data_transformations-*.whl jobs/citibike_distance_calculation.py resources/*.csv out/citibike
+#poetry run spark-submit --master local --py-files dist/data_transformations-*.whl jobs/citibike_ingest.py resources/citibike/*.csv out/citibike
+poetry run spark-submit --master local --py-files dist/data_transformations-*.whl jobs/citibike_distance_calculation.py out/citibike/*.parquet out/citibike_distance
 
 cat project.log
 
